def6742eacf8e562cd6958056a4e484164712e65
shivakrishnavv11 <shivakrishna.vv11@gmail.com>
2026-03-18 09:16:38 +0000

    version update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 http.createServer((req,res)=>{
-res.write("Automated CI/CD Project Version 1");
+res.write("Automated CI/CD Project Version 2");
 res.end();
 }).listen(3000);
